ee0a420b6716d5ec75aed2ed776acde034b5b1bb
Brett Kessler <brettkessler1@gmail.com>
2018-02-26 20:02:16 -0600

    Second Part Added

--- js/fizz-buzz.js
+++ js/fizz-buzz.js
@@ -2,8 +2,16 @@ $( document ).ready(function() {
 
     function getNumbers(nums){
 		// iterates through these to determine if fiz, buzz or fizzbuzz.
-		if(nums % 15 === 0){
+		if(nums % 105 === 0){
+			return 'Fizz Buzz Pop'
+		} else if (nums % 35 === 0){
+			return 'Buzz Pop'
+		} else if (nums % 21 === 0){
+			return 'Fizz Pop'
+		}  else if (nums % 15 === 0){
 			return 'Fizz Buzz'
+		} else if (nums % 7 === 0){
+			return 'Pop'
 		} else if(nums % 5 === 0 ){
 			return 'Buzz'
 		} else if(nums % 3 === 0){
